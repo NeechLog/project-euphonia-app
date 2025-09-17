2995471607e43e2c8900b3d3bc4b284cf5688a80
Gagan Bajpai <193398453+GaganBajpai@users.noreply.github.com>
2025-09-17 21:32:26 +0530

    making local_adapter package.

---
+++ api/local_adapter/__init__.py
@@ -0,0 +1,8 @@
+"""
+Local adapter package for project-euphonia-app.
+
+This package contains local implementations of various adapters and utilities
+that interface with local resources and services.
+"""
+
+__version__ = '0.1.0'
