325aa9ebfcb18c1571d450816da2decc0019f16a
= <193398453+GaganBajpai@users.noreply.github.com>
2025-12-06 08:10:44 +0530

    Parameter name incorrect.

--- api/start_server.py
+++ api/start_server.py
@@ -80,7 +80,7 @@ def start_server(background=True):
         config_dir = os.environ.get('AUTH_CONFIG_DIR')
         if config_dir:
             print(f"Using auth config from: {config_dir}")
-            init_auth_config(config_dir=Path(config_dir))
+            init_auth_config(base_dir=Path(config_dir))
         else:
             init_auth_config()
         print("AuthConfig initialized successfully")
